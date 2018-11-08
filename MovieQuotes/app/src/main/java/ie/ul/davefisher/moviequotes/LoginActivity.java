cd422634edc860d3af92ebd39bc880da9dcc0e11
David Fisher <fisherds@rose-hulman.edu>
2018-11-08 11:58:18 +0000

    register a new user

--- MovieQuotes/app/src/main/java/ie/ul/davefisher/moviequotes/LoginActivity.java
+++ MovieQuotes/app/src/main/java/ie/ul/davefisher/moviequotes/LoginActivity.java
@@ -1,16 +1,31 @@
 package ie.ul.davefisher.moviequotes;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+  private FirebaseAuth mAuth;
+  private EditText mEmailEditText;
+  private EditText mPasswordEditText;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+    mEmailEditText = findViewById(R.id.email_edittext);
+    mPasswordEditText = findViewById(R.id.password_edittext);
+    mAuth = FirebaseAuth.getInstance();
   }
 
   public void handleSignIn(View view) {
@@ -18,7 +33,36 @@ public class LoginActivity extends AppCompatActivity {
   }
 
   public void handleSignUp(View view) {
-    Toast.makeText(this, "Sign Up", Toast.LENGTH_SHORT).show();
+    String email = mEmailEditText.getText().toString();
+    String password = mPasswordEditText.getText().toString();
 
+    if (email.length() < 5 || !email.contains("@")) {
+      mEmailEditText.setError(getString(R.string.invalid_email));
+    } else if (password.length() < 6) {
+      mPasswordEditText.setError(getString(R.string.invalid_password));
+    } else {
+      mAuth.createUserWithEmailAndPassword(email, password)
+          .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+              if (task.isSuccessful()) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+              } else {
+                Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+              }
+            }
+          });
+    }
   }
 }
+
+
+
+
+
+
+
+
+
+
