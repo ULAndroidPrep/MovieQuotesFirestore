c0c5b15f3b5c81214453b9b55bcf806f9efaf5bb
David Fisher <fisherds@rose-hulman.edu>
2018-11-08 11:48:14 +0000

    LoginActivity xml

--- MovieQuotes/app/src/main/java/ie/ul/davefisher/moviequotes/LoginActivity.java
+++ MovieQuotes/app/src/main/java/ie/ul/davefisher/moviequotes/LoginActivity.java
@@ -2,6 +2,8 @@ package ie.ul.davefisher.moviequotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class LoginActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+  }
+
+  public void handleSignIn(View view) {
+    Toast.makeText(this, "Sign in", Toast.LENGTH_SHORT).show();
+  }
+
+  public void handleSignUp(View view) {
+    Toast.makeText(this, "Sign Up", Toast.LENGTH_SHORT).show();
+
   }
 }
